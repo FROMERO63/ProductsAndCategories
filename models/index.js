e2a8c09f19678a808d4d9d25d19b439466b47e73
FROMERO63 <romerofrank63@gmail.com>
2023-10-30 17:25:59 -0700

    edited foreign key references in the index.js

--- models/index.js
+++ models/index.js
@@ -7,23 +7,29 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.hasOne(Category, {
   foreignKey: 'id',
-  onDelete: 'CASCADE',
+  onDelete: 'SET NULL',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'SET NULL',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'product_tag'
+  through: 'product_tag',
+  foreignKey: {
+    name: 'id'
+  }
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'product_tag'
+  through: 'product_tag',
+  foreignKey: {
+    name: 'id'
+  }
 });
 
 module.exports = {
